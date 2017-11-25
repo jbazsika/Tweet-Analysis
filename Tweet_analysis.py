9aa6871d23056c29502b86ba85b5ffb76089b1f2
jbazsika <jbazsika@users.noreply.github.com>
2017-11-24 17:24:44 -0700

    Update Tweet_analysis.py

--- Tweet_analysis.py
+++ Tweet_analysis.py
@@ -1,7 +1,10 @@
+#Use this python script to generate tweets from Twitter. 
+#Use for tweet sentiment analysis
 import tweepy
 import csv
 import pandas as pd
-####input your credentials here
+####input your credentials here. 
+####You will need to register on Twitter to get the credetials under Twitter Developer.(https://apps.twitter.com)
 consumer_key = ''
 consumer_secret = ''
 access_token = ''
@@ -12,11 +15,11 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
 # Open/Create a file to append data
-csvFile = open('AFINN-111.csv', 'a')
+csvFile = open('AFINN-111.csv', 'a')#http://www2.imm.dtu.dk/pubdb/views/publication_details.php?id=6010
 #Use csv Writer
 csvWriter = csv.writer(csvFile)
 
-for tweet in tweepy.Cursor(api.search,q="#ANTIFA",count=100,
+for tweet in tweepy.Cursor(api.search,q="#ANTIFA",count=100,#Use any search term you'd like
                            lang="en",
                            since="2017-04-03").items():
     print (tweet.created_at, tweet.text)
